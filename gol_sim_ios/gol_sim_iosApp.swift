9cd30f00465b474227ab669c4beb738b84ba6871
Steven Blythe <blythesr@googlemail.com>
2021-09-29 18:17:05 +0100

    Initial Commit

---
+++ gol_sim_ios/gol_sim_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  gol_sim_iosApp.swift
+//  gol_sim_ios
+//
+//  Created by Steve Blythe on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct gol_sim_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
